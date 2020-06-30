9c66204a7bf6a9379b289aeccc66ed2fab7bc003
cahsegraffeo <chasegraffeo@gmail.com>
2020-06-30 00:49:22 -0500

    edit

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,8 +3,8 @@ import { useHistory } from "react-router-dom";
 import { FaRebel } from "react-icons/fa";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-//import Button from 'react-bootstrap/button'
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from 'react-bootstrap/button'
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Navigation() {
   const history = useHistory();
@@ -21,20 +21,11 @@ function Navigation() {
         </h1>
       </Container>
 
-      <NavDropdown title="More" id="dropdown">
-        <NavDropdown.Item className="text-warning" onClick={() => history.push("/")} id="item">
-          HOME
-        </NavDropdown.Item>
-        <NavDropdown.Item className="text-warning"  onClick={() => history.push("/Resume")} id="item">
-          RESUME
-        </NavDropdown.Item>
-        <NavDropdown.Item className="text-warning"  onClick={() => history.push("/Projects")} id="item">
-          PROJECTS
-        </NavDropdown.Item>
+     
         
         {/* <Button onClick={() => history.push("/")} className="btn btn-warning">Home</Button> */}
-        {/* <Button onClick={() => history.push("/Views/Resume/Resume")} className="btn btn-warning">Resume</Button> */}
-      </NavDropdown>
+        <Button onClick={() => history.push("/Projects")} className="btn btn-warning">Projects</Button>
+     
     </Navbar>
   );
 }
